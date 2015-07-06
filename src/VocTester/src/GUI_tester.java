1b9f737240e7e96bc2626409330841198af5f395
Dennis <dennis.violante@yahoo.de>
2015-07-06 11:27:03 +0200

    Rearrangement of the Project

--- src/VocTester/src/GUI_tester.java
+++ src/VocTester/src/GUI_tester.java
@@ -3,8 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package joanaklausurabfrager;
-
 import java.awt.Color;
 
 import javax.xml.namespace.QName;
--- src/VocTester/src/InputOutput.java
+++ src/VocTester/src/InputOutput.java
@@ -3,8 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package joanaklausurabfrager;
-
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
